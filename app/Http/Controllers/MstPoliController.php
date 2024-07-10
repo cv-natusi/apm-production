0022e844d9537b59574c11ed07bfbd7fc39b4899
dwialim <dweialim@gmail.com>
2024-07-10 09:09:50 +0700

    update poli btk

--- app/Http/Controllers/MstPoliController.php
+++ app/Http/Controllers/MstPoliController.php
@@ -8,79 +8,86 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Models\Rsu_Bridgingpoli;
 use App\Http\Models\rsu_poli;
-use App\Http\Models\Poli;
 use App\Http\Models\KodeAwalanPoli;
 use Redirect, Validator, Datatables, DB, Auth, DateTime;
 
 class MstPoliController extends Controller{
-    public function index(Request $request){
-        return view('Admin.mstpoli.main');
-    }
+	public function index(Request $request){
+		return view('Admin.mstpoli.main');
+	}
 
-    public function listMstPoli(Request $request){
-        if ($request->ajax()) {
-            $data = Rsu_Bridgingpoli::has('tm_poli')
-                ->with('tm_poli.kode_awalan_poli')
-                ->groupBy('mapping_poli_bridging.kdpoli_rs')
-                ->orderBy('kdpoli_rs','asc')
-                ->get();
+	public function listMstPoli(Request $request){
+		if ($request->ajax()) {
+			$data = Rsu_Bridgingpoli::has('tm_poli')
+				->with('tm_poli.kode_awalan_poli')
+				->groupBy('mapping_poli_bridging.kdpoli_rs')
+				->orderBy('kdpoli_rs','asc')
+				->get();
+			return Datatables::of($data)
+				->addIndexColumn()
+				->addColumn('kode',function($row){
+					$kode = $row->tm_poli->kode_awalan_poli;
+					$kode = !empty($kode)?$kode->kode_awal:'-';
+					return $kode;
+				})
+				->make(true);
+		}
+		return view('Admin.mstpoli.main');
+	}
 
-            return Datatables::of($data)
-            ->addIndexColumn()
-            ->addColumn('kode',function($row){
-                $kode = $row->tm_poli->kode_awalan_poli;
-                $kode = !empty($kode)?$kode->kode_awal:'-';
-                return $kode;
-            })
-            ->make(true);
-        }
-        return view('Admin.mstpoli.main');
-    }
+	public function form(Request $request){
+		if (!empty($request->id)) {
+			$data['data'] = Rsu_Bridgingpoli::has('tm_poli')
+				->with('tm_poli.kode_awalan_poli')
+				->groupBy('mapping_poli_bridging.kdpoli_rs')
+				->where('mapping_poli_bridging.kdpoli_rs', $request->id)
+				->first();
+		}
+		$data['poli'] = Rsu_Bridgingpoli::has('tm_poli')
+			->with('tm_poli')
+			->groupBy('mapping_poli_bridging.kdpoli_rs')
+			->orderBy('kdpoli_rs','asc')
+			->get();
+		$content = view('Admin.mstpoli.form', $data)->render();
+		return ['status' => 'success', 'content' => $content, 'data' => $data];
+	}
 
-    public function form(Request $request){
-        if (!empty($request->id)) {
-            $data['data'] = Rsu_Bridgingpoli::has('tm_poli')
-                ->with('tm_poli.kode_awalan_poli')
-                ->groupBy('mapping_poli_bridging.kdpoli_rs')
-                ->where('mapping_poli_bridging.kdpoli_rs', $request->id)
-                ->first();
-        }
-
-        $data['poli'] = Rsu_Bridgingpoli::has('tm_poli')
-            ->with('tm_poli')
-            ->groupBy('mapping_poli_bridging.kdpoli_rs')
-            ->orderBy('kdpoli_rs','asc')
-            ->get();
-        
-        $content = view('Admin.mstpoli.form', $data)->render();
-        return ['status' => 'success', 'content' => $content, 'data' => $data];
-    }
-
-    public function store(Request $request){
-        $kodeAwalan = KodeAwalanPoli::where('kdpoli_rs', $request->id)->first();
-        $cek = 'lama';
-        if (empty($kodeAwalan)) {
-            $cek = 'baru';
-            $kodeAwalan = new KodeAwalanPoli;
-        }
-        $getKdPoli = Rsu_Bridgingpoli::where('kdpoli_rs', $request->poli)->first();
-
-        $kodeAwalan->kdpoli    = strtoupper($getKdPoli->kdpoli);
-        $kodeAwalan->kdpoli_rs = strtoupper($request->poli);
-        $kodeAwalan->kode_awal = strtoupper($request->kodeawalan);
-        $kodeAwalan->save();
-        if($kodeAwalan){
-            return [
-                'status' => 'success',
-                'code'   => 200,
-                'message'   => ($cek=='baru'?'Data berhasil disimpan':'Data berhasil diperbarui'),
-            ];
-        }else{
-            return [
-                'status' => 'error',
-                'code'   => 400,
-                'message'   => ($cek=='baru'?'Data gagal disimpan':'Data gagal diperbarui'),
-            ];
-        }
-    }
+	public function store(Request $request){
+		$request->merge([
+			'poli' => strtoupper($request->poli),
+			'kodeawalan' => strtoupper($request->kodeawalan),
+		]);
+		if(($cekDuplikat = KodeAwalanPoli::where('kode_awal', $request->kodeawalan)->first()) && $cekDuplikat->kdpoli_rs != $request->id){
+			$poli = rsu_poli::where('KodePoli',$cekDuplikat->kdpoli_rs)->first();
+			$namaPoli = $poli ? $poli->NamaPoli : 'poli lain';
+			return [
+				'status' => 'error',
+				'code' => 400,
+				'message' => "Kode sudah digunakan untuk $namaPoli",
+			];
+		}
+		$cek = 'lama';
+		if(!($kodeAwalan = KodeAwalanPoli::where('kdpoli_rs', $request->id)->first())){
+			$cek = 'baru';
+			$kodeAwalan = new KodeAwalanPoli;
+		}
+		$getKdPoli = Rsu_Bridgingpoli::where('kdpoli_rs', $request->poli)->first();
+		$kodeAwalan->kdpoli = strtoupper($getKdPoli->kdpoli);
+		$kodeAwalan->kdpoli_rs = strtoupper($request->poli);
+		$kodeAwalan->kode_awal = strtoupper($request->kodeawalan);
+		$kodeAwalan->save();
+		if($kodeAwalan){
+			return [
+				'status' => 'success',
+				'code' => 200,
+				'message' => ($cek=='baru'?'Data berhasil disimpan':'Data berhasil diperbarui'),
+			];
+		}else{
+			return [
+				'status' => 'error',
+				'code' => 400,
+				'message' => ($cek=='baru'?'Data gagal disimpan':'Data gagal diperbarui'),
+			];
+		}
+	}
 }
--- resources/views/Admin/mstpoli/form.blade.php
+++ resources/views/Admin/mstpoli/form.blade.php
@@ -1,45 +1,43 @@
 <div class="content col-lg-12 col-md-12 col-sm-12 col-xs-12" style="min-height: 0px;">
-    <div class="box box-primary add-form">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <form class="formAdd">
-                    <input type="hidden" name="id" value="{{ (!empty($data)) ? $data->kdpoli_rs : '' }}">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <label>NAMA POLI</label>
-                            <select class="form-control" name="poli" id="poli" >
-                                <option value="">- PILIH POLI -</option>
-                                @if($poli->count()!=0)
-                                    @foreach ($poli as $p)
-                                        <option @if(!empty($data) && $p->kdpoli_rs == $data->kdpoli_rs) selected @endif value="{{$p->kdpoli_rs}}">{{$p->tm_poli->NamaPoli}}</option>
-                                    @endforeach
-                                @endif
-                            </select>
-                        </div>
-                        <div class="col-md-12">
-                            <label>Kode Awalan</label>
-                            <input type="text" name="kodeawalan" class="form-control" value="{{ !empty($data->tm_poli->kode_awalan_poli->kode_awal) ? $data->tm_poli->kode_awalan_poli->kode_awal : ''}}">
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="panel-footer">
-                <button type="button" class="btn btn-sm btn-warning btn-cancel">Kembali</button>
-                <input type="submit" value="Simpan" class="btn btn-sm btn-success btn-store">
-            </div>
-        </div>
-    </div>
+	<div class="box box-primary add-form">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<form class="formAdd">
+					<input type="hidden" name="id" value="{{ (!empty($data)) ? $data->kdpoli_rs : '' }}">
+					<div class="row">
+						<div class="col-md-12">
+							<label>NAMA POLI</label>
+							<select class="form-control" name="poli" id="poli" >
+								<option value="">- PILIH POLI -</option>
+								@if($poli->count()!=0)
+								@foreach ($poli as $p)
+								<option @if(!empty($data) && $p->kdpoli_rs == $data->kdpoli_rs) selected @endif value="{{$p->kdpoli_rs}}">{{$p->tm_poli->NamaPoli}}</option>
+								@endforeach
+								@endif
+							</select>
+						</div>
+						<div class="col-md-12">
+							<label>Kode Awalan</label>
+							<input type="text" name="kodeawalan" class="form-control" value="{{ !empty($data->tm_poli->kode_awalan_poli->kode_awal) ? $data->tm_poli->kode_awalan_poli->kode_awal : ''}}">
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="panel-footer">
+				<button type="button" class="btn btn-sm btn-warning btn-cancel">Kembali</button>
+				<input type="submit" value="Simpan" class="btn btn-sm btn-success btn-store">
+			</div>
+		</div>
+	</div>
 </div>
 
 <script type="text/javascript">
-    // $("#poli").select2();
-    
-    $(document).ready(function() {
-        $('select').selectpicker();
-    });
+	$(document).ready(function() {
+		$('select').selectpicker();
+	});
 
-    // BTN KEMBALI
-    $('.btn-cancel').click(function(e) {
+	// BTN KEMBALI
+	$('.btn-cancel').click(function(e) {
 		e.preventDefault();
 		$('.add-form').fadeOut();
 		$('.other-page').fadeOut(function() {
@@ -50,86 +48,77 @@
 
 	// BTN SIMPAN
 	$('.btn-store').click(function(e) {
-        e.preventDefault();
-        var data = new FormData($('.formAdd')[0]);
-        $.ajax({
-            url: "{{route('mstpoli-store')}}",
-            type: 'POST',
-            data: data,
-            async: true,
-            cache: false,
-            contentType: false,
-            processData: false
-        }).done(function(data) {
-            if (data.code == 200) {
-                swal("Success!", data.message, data.status);
-                $('.other-page').fadeOut(function() {
-                    $('.other-page').empty();
-                    $('.main-layer').fadeIn();
-                    $('#dataTable').DataTable().ajax.reload();
-                });
-            }else{
-                swal('Gagal!', data.message, data.status);
-            }
-        }).fail(function() {
-            swal("MAAF!", "Terjadi Kesalahan, Silahkan Ulangi Kembali !!", "warning");
-        });
-    });
+		e.preventDefault();
+		var data = new FormData($('.formAdd')[0]);
+		$.ajax({
+			url: "{{route('mstpoli-store')}}",
+			type: 'POST',
+			data: data,
+			async: true,
+			cache: false,
+			contentType: false,
+			processData: false
+		}).done(function(data) {
+			if (data.code == 200) {
+				swal("Success!", data.message, data.status);
+				$('.other-page').fadeOut(function() {
+					$('.other-page').empty();
+					$('.main-layer').fadeIn();
+					$('#dataTable').DataTable().ajax.reload();
+				});
+			}else{
+				swal('Gagal!', data.message, data.status);
+			}
+		}).fail(function() {
+			swal("MAAF!", "Terjadi Kesalahan, Silahkan Ulangi Kembali !!", "warning");
+		});
+	});
 
-    // ADD POLI
-    $('#add').click(function() {
-        // console.log('tombol diklik')
-        var id = $('#poli_id').val();
-        var tindakan = '';
-        $.ajax({
-            url: "{{ route('get-poli') }}",
-            type: 'POST',
-            data: {
-                id: id
-            },
-        }).done(function(data) {
-            $.each(data, function (index, v) { 
-                var kdpoli = v.KodePoli;
-                var nmpoli = v.NamaPoli;
+	// ADD POLI
+	$('#add').click(function() {
+		var id = $('#poli_id').val();
+		var tindakan = '';
+		$.ajax({
+			url: "{{ route('get-poli') }}",
+			type: 'POST',
+			data: {
+				id: id
+			},
+		}).done(function(data) {
+			$.each(data, function (index, v) { 
+				var kdpoli = v.KodePoli;
+				var nmpoli = v.NamaPoli;
+				// var html = ''
+				// html += '<tr id="kd_'+kdpoli+'">'
+				// html += '<td>'+kdpoli+'</td>'
+				// html += '<td>'+nmpoli+'</td>'
+				// html += '<td>'
+				// html += '<button type="button" class="btn btn-danger" onclick="del_poli(+kdpoli+)"><i class="fa fa-times"></i></button>'
+				// html += '</td>'
+				// html += '</tr>'
+				var markup = "<tr id='kd_"+kdpoli+"'><td>" + kdpoli + "</td><td>" + nmpoli + "</td><td><button type='button' class='btn btn-danger' onclick='del_row_poli("+kdpoli+")'><i class='fa fa-times'></i></button></td></tr>";
+				$("#tempatData").append(markup);
+			});
+		}).fail(function() {
+			Swal.fire("MAAF!", "Terjadi Kesalahan, Silahkan Ulangi Kembali !!", "warning");
+		});
+	});
 
-                // var html = ''
-                
-                // html += '<tr id="kd_'+kdpoli+'">'
-                // html += '<td>'+kdpoli+'</td>'
-                // html += '<td>'+nmpoli+'</td>'
-                // html += '<td>'
-                // html += '<button type="button" class="btn btn-danger" onclick="del_poli(+kdpoli+)"><i class="fa fa-times"></i></button>'
-                // html += '</td>'
-                // html += '</tr>'
+	// DELETE ROW
+	function del_row_poli(kdpoli) {
+		$('#kd_'+kdpoli).remove();
+	}
 
-                var markup = "<tr id='kd_"+kdpoli+"'><td>" + kdpoli + "</td><td>" + nmpoli + "</td><td><button type='button' class='btn btn-danger' onclick='del_row_poli("+kdpoli+")'><i class='fa fa-times'></i></button></td></tr>";
-                $("#tempatData").append(markup);
-            });
-
-            // ADD THE ICDX TO TABLE
-            
-
-        }).fail(function() {
-            Swal.fire("MAAF!", "Terjadi Kesalahan, Silahkan Ulangi Kembali !!", "warning");
-        });
-    });
-
-    // DELETE ROW
-    function del_row_poli(kdpoli) {
-        $('#kd_'+kdpoli).remove();
-    }
-
-    // DELETE POLI
-    function del_poli(id) {
-        // console.log(id)
-        $.post("{{route('delete-poli')}}", {id:id}).done((data) => {
-            if (data.status == 'success') {
-                $('#kd_'+id).remove();
-                
-                swal(data.status, data.message, data.title);
-            } else {
-                swal(data.status, data.message, data.title);
-            }
-        })
-    }
+	// DELETE POLI
+	function del_poli(id) {
+		$.post("{{route('delete-poli')}}", {id:id}).done((data) => {
+			if (data.status == 'success') {
+				$('#kd_'+id).remove();
+				
+				swal(data.status, data.message, data.title);
+			} else {
+				swal(data.status, data.message, data.title);
+			}
+		})
+	}
 </script>
