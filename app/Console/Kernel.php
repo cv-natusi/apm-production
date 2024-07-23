bb4b31be8d11e093efa7702dc7050545b340d7ec
dwialim <dweialim@gmail.com>
2024-07-23 08:47:15 +0700

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,33 +5,26 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        \App\Console\Commands\Inspire::class,
-        \App\Console\Commands\ReminderControl::class,
-    ];
+class Kernel extends ConsoleKernel{
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		\App\Console\Commands\Inspire::class,
+		\App\Console\Commands\ReminderControl::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('inspire')
-                 ->hourly();
-        $schedule->command('reminder:control')
-                 ->dailyAt('01:00');
-    }
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('session:prune')->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule){
+		$schedule->command('inspire')->hourly();
+		$schedule->command('reminder:control')->dailyAt('01:00');
+		$schedule->command('session:prune')->hourly();
+	}
 }
