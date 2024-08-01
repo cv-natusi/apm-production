f6ea8c8364d1c4496272faf63b29c46566a97553
dwialim <dweialim@gmail.com>
2024-08-01 08:28:30 +0700

    wablas

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -116,6 +116,10 @@ class WablasController extends Controller{
 			$cekPas = $dataBot->pasien_baru;
 			$data['phone'] = $dataBot->phone;
 			$bool = $timeCur2<$jamBuka && $tglBerobat==$dateCur;
+			if($dataPasien->kodePoli=='017' && $tglBerobat=='2024-08-01'){
+				$txt = "<p style='text-align:center; font-weight:600; font-size:2.5rem; margin-top:4%;'>MOHON MAAF, KUOTA POLI SAAT ONKOLOGI SAAT INI SUDAH PENUH</p>";
+				return $txt;
+			}
 			if($tglBerobat > $dateCur || $bool){
 				$data['pesan1'] = "Mohon Maaf URL Belum Aktif!";
 				$data['pesan2'] = $bool ? "URL akan aktif pada jam $jamBuka WIB" : "URL hanya berlaku pada tanggal yang didaftarkan!";
