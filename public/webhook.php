c72f3bca6bc8875252c44d75ee69ca0a6360b71f
dwialim <dweialim@gmail.com>
2024-08-06 09:26:27 +0700

    update webhook

--- public/webhook.php
+++ public/webhook.php
@@ -382,17 +382,21 @@ if((preg_match("/^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}$/",$waText
 
 					$cekBatpas = "SELECT * FROM bot_data_pasien WHERE cust_id='$idPsn' AND tglBerobat='$tglBerobat' AND masukMaster='belum'";
 					$resBatpas = mysqli_query($wablas,$cekBatpas);
-					// if($phone=='6281335537942'){
-					// 	if(in_array(date('D',strtotime($tglBerobat)), ['Tue','Thu'])){
-					// 		echo 'masuk';die();
-					// 	}
-					// 	echo date('D-m-Y',strtotime($tglBerobat));
-					// 	// $notIn = "'ALG','UGD','ANU','GIG'$notInSementara";
-					// 	// $poli = "SELECT tp.NamaPoli,mp.kdpoli_rs,mp.kdpoli FROM mapping_poli_bridging AS mp JOIN tm_poli AS tp ON mp.kdpoli_rs=tp.KodePoli WHERE mp.kdpoli NOT IN ($notIn) GROUP BY mp.kdpoli_rs ORDER BY tp.KodePoli ASC"; # GIG=="poli gigi dokter umum"
-					// 	// $resPoli = mysqli_query($dbrsud,$poli);
-					// 	// echo json_encode($resPoli->fetch_all(MYSQLI_ASSOC),JSON_PRETTY_PRINT);
-					// 	die();
-					// }
+					if($phone=='6281335537942'){
+						$getIgnorePoli = ignorePoli($request);
+						$notIn = "$getIgnorePoli";
+						// $poli = "SELECT tp.NamaPoli,mp.kdpoli_rs,mp.kdpoli FROM mapping_poli_bridging AS mp JOIN tm_poli AS tp ON mp.kdpoli_rs=tp.KodePoli WHERE mp.kdpoli NOT IN ('ALG','UGD','ANU') GROUP BY mp.kdpoli_rs ORDER BY tp.KodePoli ASC";
+						$poli = "SELECT tp.NamaPoli,mp.kdpoli_rs,mp.kdpoli FROM mapping_poli_bridging AS mp JOIN tm_poli AS tp ON mp.kdpoli_rs=tp.KodePoli WHERE mp.kdpoli NOT IN ($notIn) GROUP BY mp.kdpoli_rs ORDER BY tp.KodePoli ASC"; # GIG=="poli gigi dokter umum"
+						$resPoli = mysqli_query($dbrsud,$poli);
+						$msg = getPoli($resPoli);
+                  echo $msg;
+						// echo date('D-m-Y',strtotime($tglBerobat));
+						// $notIn = "'ALG','UGD','ANU','GIG'$notInSementara";
+						// $poli = "SELECT tp.NamaPoli,mp.kdpoli_rs,mp.kdpoli FROM mapping_poli_bridging AS mp JOIN tm_poli AS tp ON mp.kdpoli_rs=tp.KodePoli WHERE mp.kdpoli NOT IN ($notIn) GROUP BY mp.kdpoli_rs ORDER BY tp.KodePoli ASC"; # GIG=="poli gigi dokter umum"
+						// $resPoli = mysqli_query($dbrsud,$poli);
+						// echo json_encode($resPoli->fetch_all(MYSQLI_ASSOC),JSON_PRETTY_PRINT);
+						die();
+					}
 					if($resBatpas->num_rows>0){
 						if($statusChat==23){
 							$dataIn['status'] = 24;
